e4709b83f301c5d4314e59bc317d7e1a28ee8418
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-08-29 12:11:22 +0200

    Test SonarCloud

---
+++ server/sonar-test.js
@@ -0,0 +1,23 @@
+function duplicateCodeExample1() {
+    let x = 5;
+    let y = 10;
+    let result = x + y;
+    console.log(result);
+    console.log(result);
+    console.log(result);
+    console.log(result);
+    console.log(result);
+    console.log(result);
+}
+
+function duplicateCodeExample2() {
+    let x = 5;
+    let y = 10;
+    let result = x + y;
+    console.log(result);
+    console.log(result);
+    console.log(result);
+    console.log(result);
+    console.log(result);
+    console.log(result);
+}
